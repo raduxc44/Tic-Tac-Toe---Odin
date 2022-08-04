75cfee1c7a9aa27c29d9b8d949df243b02480ed3
raduxc44 <raduxc44@gmail.com>
2022-08-04 03:17:40 +0300

    Reworked styling, drawing is fully functional

--- src/BoardApp.jsx
+++ src/BoardApp.jsx
@@ -13,24 +13,44 @@ function Gameboard () {
             let currentValue = currentTurn === 0 ? 'X' : 'O';
             data[index - 1] = currentValue;
             event.target.innerText = currentValue;
+
+            let tiles = document.getElementsByClassName('tile')
+            if(currentTurn === 0) {tiles[index - 1].classList.add('neon-text-x')}
+            else if(currentTurn === 1) {tiles[index - 1].classList.add('neon-text-o')}
+
             setCurrentTurn(currentTurn === 0 ? 1 : 0)
-            console.log(currentTurn)
 
         }
     }
 
     return(
-        <div className="board">
-            <div className="tile" onClick= {(e) => {draw(e, 1)}}></div>
-            <div className="tile" onClick= {(e) => {draw(e, 2)}}></div>
-            <div className="tile" onClick= {(e) => {draw(e, 3)}}></div>
-            <div className="tile" onClick= {(e) => {draw(e, 4)}}></div>
-            <div className="tile" onClick= {(e) => {draw(e, 5)}}></div>
-            <div className="tile" onClick= {(e) => {draw(e, 6)}}></div>
-            <div className="tile" onClick= {(e) => {draw(e, 7)}}></div>
-            <div className="tile" onClick= {(e) => {draw(e, 8)}}></div>
-            <div className="tile" onClick= {(e) => {draw(e, 9)}}></div>
+        <div className="main-container">
+            <div className="reset-container">
+                <button className="reset-butt">RESET</button>
+            </div>
+            <div className="board">
+                <div className="tile" onClick= {(e) => {draw(e, 1)}}></div>
+                <div className="tile" onClick= {(e) => {draw(e, 2)}}></div>
+                <div className="tile" onClick= {(e) => {draw(e, 3)}}></div>
+                <div className="tile" onClick= {(e) => {draw(e, 4)}}></div>
+                <div className="tile" onClick= {(e) => {draw(e, 5)}}></div>
+                <div className="tile" onClick= {(e) => {draw(e, 6)}}></div>
+                <div className="tile" onClick= {(e) => {draw(e, 7)}}></div>
+                <div className="tile" onClick= {(e) => {draw(e, 8)}}></div>
+                <div className="tile" onClick= {(e) => {draw(e, 9)}}></div>
+            </div>
+            <div className="info">
+                <div className="player">
+                    <div className="player-info"><h2>Player 1 :</h2></div>
+                    <div className="neon-text-x">X</div>
+                </div>
+                <div className="player">
+                    <div className="player-info"><h2>Player 2 :</h2></div>
+                    <div className="neon-text-o">O</div>
+                </div>
+            </div>
         </div>
+       
     )
 }
 
